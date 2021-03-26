03d601f68377c6257bedafb495c4578a922d503c
Jakub Dulas <j-dulas@student.newbury-college.ac.uk>
2021-03-26 15:02:12 +0000

    1

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,96 +22,76 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.FontDialog1 = New System.Windows.Forms.FontDialog()
-        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Password = New System.Windows.Forms.TextBox()
+        Me.UserName = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.TabControl1 = New System.Windows.Forms.TabControl()
-        Me.TabPage1 = New System.Windows.Forms.TabPage()
-        Me.TabPage2 = New System.Windows.Forms.TabPage()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.TabControl1.SuspendLayout()
-        Me.TabPage1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'ToolStrip1
+        'Label1
         '
-        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
-        Me.ToolStrip1.Name = "ToolStrip1"
-        Me.ToolStrip1.Size = New System.Drawing.Size(800, 25)
-        Me.ToolStrip1.TabIndex = 0
-        Me.ToolStrip1.Text = "ToolStrip1"
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(36, 96)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 15)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(24, 193)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 15)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Password"
+        '
+        'Password
+        '
+        Me.Password.Location = New System.Drawing.Point(120, 185)
+        Me.Password.Name = "Password"
+        Me.Password.Size = New System.Drawing.Size(153, 23)
+        Me.Password.TabIndex = 3
+        '
+        'UserName
+        '
+        Me.UserName.AcceptsReturn = True
+        Me.UserName.AccessibleName = "Please input your User Name"
+        Me.UserName.HideSelection = False
+        Me.UserName.Location = New System.Drawing.Point(120, 96)
+        Me.UserName.Name = "UserName"
+        Me.UserName.Size = New System.Drawing.Size(153, 23)
+        Me.UserName.TabIndex = 4
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(68, 87)
+        Me.Button1.Location = New System.Drawing.Point(198, 287)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "ta"
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
-        '
-        'TabControl1
-        '
-        Me.TabControl1.Controls.Add(Me.TabPage1)
-        Me.TabControl1.Controls.Add(Me.TabPage2)
-        Me.TabControl1.Location = New System.Drawing.Point(0, 135)
-        Me.TabControl1.Name = "TabControl1"
-        Me.TabControl1.SelectedIndex = 0
-        Me.TabControl1.Size = New System.Drawing.Size(800, 312)
-        Me.TabControl1.TabIndex = 2
-        '
-        'TabPage1
-        '
-        Me.TabPage1.Controls.Add(Me.Button2)
-        Me.TabPage1.Location = New System.Drawing.Point(4, 24)
-        Me.TabPage1.Name = "TabPage1"
-        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(792, 419)
-        Me.TabPage1.TabIndex = 0
-        Me.TabPage1.Text = "TabPage1"
-        Me.TabPage1.UseVisualStyleBackColor = True
-        '
-        'TabPage2
-        '
-        Me.TabPage2.Location = New System.Drawing.Point(4, 24)
-        Me.TabPage2.Name = "TabPage2"
-        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(192, 72)
-        Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "TabPage2"
-        Me.TabPage2.UseVisualStyleBackColor = True
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(24, 54)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(132, 40)
-        Me.Button2.TabIndex = 0
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.TabControl1)
         Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.UserName)
+        Me.Controls.Add(Me.Password)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.name = "Form1"
-        Me.Text = "fORM1"
-        Me.TabControl1.ResumeLayout(False)
-        Me.TabPage1.ResumeLayout(False)
+        Me.Text = "login"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents FontDialog1 As FontDialog
-    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Password As TextBox
+    Friend WithEvents UserName As TextBox
     Friend WithEvents Button1 As Button
-    Friend WithEvents TabControl1 As TabControl
-    Friend WithEvents TabPage1 As TabPage
-    Friend WithEvents Button2 As Button
-    Friend WithEvents TabPage2 As TabPage
 End Class
--- Form1.vb
+++ Form1.vb
@@ -4,9 +4,20 @@
     Private Sub TabPage1_Click(sender As Object, e As EventArgs)
 
     End Sub
+    Dim a As Integer = 0
 
-    Private Sub TabPage2_Click(sender As Object, e As EventArgs)
-        TabPage = 2
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Not UserName.Text = "" Then
+            a += 1
+
+        End If
+        If Not Password.Text - "" Then
+            a += 1
+        End If
+        If a = 2 Then
+            Write "Hey, your right"
+        End If
     End Sub
+
 End Class
